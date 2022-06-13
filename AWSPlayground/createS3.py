335232cef126e9e3961093650d509f81a422416b
Frank Su <franksu@y7mail.com>
2022-06-13 16:00:03 +1000

    Wrote program to create and destroy an s3 bucket

--- AWSPlayground/createS3.py
+++ AWSPlayground/createS3.py
@@ -1,8 +1,41 @@
 import boto3
 
 
-s3 = boto3.resource('s3')
+
+s3 = boto3.client("s3")
+response = s3.list_buckets()
+
+# Output the bucket names
+print("Existing buckets:")
+for bucket in response['Buckets']:
+    print(f'{bucket["Name"]}')
 
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+s3.create_bucket(Bucket='franksu-9447-testbucket')
+
+
+response = s3.list_buckets()
+
+# print(response)
+
+for bucket in response['Buckets']:
+    if(bucket["Name"] =='franksu-9447-testbucket'):
+        print("Successfully created s3 bucket")
+
+s3.delete_bucket(Bucket='franksu-9447-testbucket')
+
+
+response = s3.list_buckets()
+
+for bucket in response['Buckets']:
+    if(bucket["Name"] =='franksu-9447-testbucket'):
+        print("Error bucket not deleted")
+        quit()
+
+print("Bucket deleted successfully")
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # COMP9447
+
+## AWSPlayground
+
+### [Script to create and destroy S3 bucket](./AWSPlayground/createS3.py)
