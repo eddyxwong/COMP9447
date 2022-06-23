26020100b89af6457ab182c88f94ed2e099f4944
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-06-23 15:40:48 +1000

    updated some comments

--- getCloudTrail.py
+++ getCloudTrail.py
@@ -3,6 +3,7 @@ import boto3
 #specify cloudtrail client
 client = boto3.client('cloudtrail')
 
+#List all event stores in the current region for this account
 def list_event_stores():
     response = client.list_event_data_stores(
     NextToken='string',
@@ -10,12 +11,14 @@ def list_event_stores():
     )
     print(response)
 
+#List all trails in the current region for this account
 def list_trails():
     response = client.list_trails(
     NextToken='string'
     )
     print(response)
 
+#Get specific trail 
 def get_trail(trail_name):
     response = client.get_trail(
     Name=trail_name
