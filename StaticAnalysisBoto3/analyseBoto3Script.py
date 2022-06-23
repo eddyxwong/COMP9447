9081e282d3c792fc204803b88dbbf0cca4df870f
Frank Su <franksu@y7mail.com>
2022-06-24 00:07:46 +1000

    More function for static analysis of boto3 script

--- StaticAnalysisBoto3/analyseBoto3Script.py
+++ StaticAnalysisBoto3/analyseBoto3Script.py
@@ -1,10 +1,37 @@
 
 import json
-from typing import Dict, List
+from typing import Dict
 
 
-def analyseScript(filepath: str) -> json:
-    getUsedServices(filepath)  
+def analysePythonScript(filepath: str) -> json:
+
+    respDict = getUsedServices(filepath)  
+
+    newPolicy = {"Version": "2012-10-17",
+                "Statement": [] }
+
+
+    newStatement = createAllowStatement()
+
+    for service in respDict:
+        if("service" == "lambda"):
+            for method in respDict[service]:
+                newAction = createAction(service, method)
+                newStatement["Action"].append(newAction)
+
+    return newPolicy
+
+
+def createAllowStatement() ->json:
+    newStatement = {"Effect": "Allow",
+                    "Action": [],
+                    "Resource": "*"}
+    
+    return newStatement
+
+
+def createAction(service: str, method: str) -> str:
+    return "\""+ service+":"+method + "\""
 
 
 
@@ -15,10 +42,10 @@ def analyseScript(filepath: str) -> json:
 
 
 '''
-def getUsedServices(filepath: str) -> Dict[str]:
+def getUsedServices(filepath: str) -> Dict[str, str]:
     file = open(filepath, 'r')
-
     # for line in file:
+
 
 
 
