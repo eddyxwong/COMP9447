fa5494dd2ecdb8a677740aa3fe07c2e308800d19
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-07-14 05:52:41 +1000

    test action

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='StaticAnalysisBoto3',
+   name='COMP9447',
    version='1.0',
    description='A useful module',
    author='Man Foo',
    author_email='foomail@foo.example',
-   packages=['StaticAnalysisBoto3', 'astStaticAnalysis', 'AWSPlayground', 'tests'],  #same as name
+   packages=['StaticAnalysisBoto3', 'astStaticAnalysis', 'AWSPlayground', 'tests'],  #Define user modules to be installed
 )
