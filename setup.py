4119d9758149237c1acfdfba10375dec958009fc
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-07-14 05:43:42 +1000

    test action

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='COMP9447',
+   name='StaticAnalysisBoto3',
    version='1.0',
    description='A useful module',
    author='Man Foo',
    author_email='foomail@foo.example',
-   packages=['COMP9447'],  #same as name
+   packages=['StaticAnalysisBoto3'],  #same as name
 )
