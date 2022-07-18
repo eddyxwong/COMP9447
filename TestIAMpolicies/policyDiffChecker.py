c57dfc1fbef90a622c4a3e8a5dd7a5da8efaf365
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-07-18 18:20:00 +1000

    Added diff checker using parliament tool

--- TestIAMpolicies/policyDiffChecker.py
+++ TestIAMpolicies/policyDiffChecker.py
@@ -1,14 +1,31 @@
 import jsondiff as jd
 from jsondiff import diff
 import json
+import jsondiff
+import parliament
+import os
+import pandas as pd
+import subprocess
+import shlex
 
+#From the policies, map it to the functions and list out what they can do
 
-f = open("policy1.json")
-g = open("policy2.json")
+json_files = [pos_json for pos_json in os.listdir() if pos_json.endswith('.json')]
 
-text1 = json.load(f)
-text2 = json.load(g)
+response = {'Policies': []}
 
+for jsonfile_name in json_files:
+    response['Policies'].append(jsonfile_name)
+    file = open(jsonfile_name)
+    jsonfile = json.load(file)
+    jsonobj = json.dumps(jsonfile)
+    policyobj = parliament.analyze_policy_string(jsonobj)
+    shellresponse = subprocess.getoutput('parliament --file {}'.format(shlex.quote(jsonfile_name)))
+    print(shellresponse)
+    if 'Unknown' not in shellresponse:
+        policy_actions= policyobj.get_allowed_actions()
+        print('policy actions for file :')
+        for y in policy_actions:
+            print(y)
 
-v = diff(text1, text2, syntax='symmetric')
-print(v)
+print(response)
