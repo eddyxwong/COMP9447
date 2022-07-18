e0c95eee6d5fb49e3d19f1bfe7c183ccce5e52e6
Frank Su <franksu@y7mail.com>
2022-07-18 16:55:42 +1000

    merge zachs work on github actions

--- astStaticAnalysis/astBoto3.py
+++ astStaticAnalysis/astBoto3.py
@@ -1,40 +1,52 @@
 import ast
 import json
 from pprint import pprint
+import argparse
 import astpretty
-import sys
-import os.path
 
-def main(argv):
-    for arg in argv[1:]:
-        print(arg)
-        with open(arg, "r") as source:
-            tree = ast.parse(source.read())
-            # astpretty.pprint(tree, show_offsets=False)
 
-            analyzer = Analyzer()
-            analyzer.visit(tree)
-            respDict = analyzer.report()
-            print(respDict)
-            
-            response = generateIAMPolicy(respDict)
-            print(json.dumps(response, sort_keys=False, indent=4))
-    
-        # with open("astTest.py", "r") as source:
-        #     tree = ast.parse(source.read())
-        #     astpretty.pprint(tree, show_offsets=False)
+'''
+arg parse 
+parsing of directorys, filter for python files in directory recursively, potentially can use walk
+python filter in built function
 
-        # analyzer = Analyzer()
-        # analyzer.visit(tree)
-        # respDict = analyzer.report()
 
-        
-        # response = generateIAMPolicy(respDict)
-        # print(json.dumps(response, sort_keys=False, indent=4))
+AST walker, find boto3 object client (extensible for resources and sessions)
+webscraper convert action to correct IAM policy version
+have existing file in repo, with mapping of actions
+beautifulsoup, pickle 
+
+vscode collab extension.
+
+'''
+
+def main():
+    with open("refactorTest.py", "r") as source:
+        tree = ast.parse(source.read())
+
+        '''
+        Code below formats the AST and prints it out
+        '''
+        # astpretty.pprint(tree, show_offsets=False)
+
+
+
+    analyzer = Analyzer()
+    analyzer.visit(tree)
+    resp = analyzer.report()
+    print(json.dumps(generateIAMPolicy(resp), sort_keys=False, indent=4))
 
 
 
 def generateIAMPolicy(respDict):
+    """Given actions extract from boto3 script generate an IAM policy
+
+    Args:
+        respDict (_type_): _description_
+
+    Returns:
+        str: IAM policy
+    """
     statementNum = 1
 
 
@@ -62,7 +74,17 @@ def generateIAMPolicy(respDict):
             statementNum +=1
             # print(respDict[service][resource])
     return newPolicy
+
+
 def convertSnakeCasetoPascalCase(string: str) -> str:
+    """function to convert a string to Pascal Case. Needed in IAM policy generation
+
+    Args:
+        string (str): any string
+
+    Returns:
+        str: converted string
+    """
 
     res = string.replace("_", " ").title().replace(" ", "")
 
@@ -71,6 +93,14 @@ def convertSnakeCasetoPascalCase(string: str) -> str:
 
 
 def createAllowStatement(resource: str, statementNum: int):
+    """Creates a statement which compose IAM policies
+
+    Args:
+        resource (str): resource ARN
+        statementNum (int): statementNum, no functional purpose
+    Returns:
+        dict : Dictionary with required structure.
+    """
     newStatement = {"Sid": "Statement" + str(statementNum),
                     "Effect": "Allow",
                     "Action": [],
@@ -83,99 +113,92 @@ def createAction(service: str, method: str) -> str:
     return "\""+ service+":"+method + "\""
 
 
-def getUserCreatedObj(node):
-        userObj = None
-        awsService = None
 
-        for Name in node.targets:
-
-            userObj = Name.id
-
-        for Constant in node.value.args:
-                awsService = Constant.value
-
-        return (userObj,awsService)
-
-def getCalledAWSMethod(node, awsService):
-    methodARN = "*"
-    for keyword in node.value.keywords:
-        if(keyword.arg == 'FunctionName'):
-            methodARN = keyword.value.value
-    return(node.value.func.attr, awsService, methodARN)
+# {'lambda': {'arn:aws:lambda:us-east-1:221094580673:function:testFunction': ['get_function'], '*': ['list_functions']}, 's3': {'*': ['list_buckets', 'create_bucket', 'delete_bucket']}}
 
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
         self.stats = {"import": [], "from": []}
+
+        self.extractDict = {}
+
+        '''
+        userObjDict provides a mapping of the user created objects and the AWS service they interact with. 
+        As example:
+        {'clientLambda': 'lambda'}
+
+        Consider classes, self references
+
+        This dictionary indicates that there is a user created object 'clientLambda' that interfaces with the lambda service
+        '''
         self.userObjDict = {}
-        self.calledMethodDict = {}
-    # def visit_Import(self, node):
-    #     for alias in node.names:
-    #         self.stats["import"].append(alias.name)
-    #     self.generic_visit(node)
 
-    # def visit_ImportFrom(self, node):
-    #     for alias in node.names:
-    #         self.stats["from"].append(alias.name)
-    #     self.generic_visit(node)
-    
-        
+    def visit_Import(self, node):
+        for alias in node.names:
+            self.stats["import"].append(alias.name)
+        self.generic_visit(node)
+
+    def visit_ImportFrom(self, node):
+        for alias in node.names:
+            self.stats["from"].append(alias.name)
+        self.generic_visit(node)
     def visit_Assign(self, node):
+        try:
+            if(node.value.func.value.id == 'boto3'):
+                self.userObjDict[node.targets[0].id] = node.value.args[0].value
+
+                if node.value.args[0].value not in self.extractDict:
+                    self.extractDict[node.value.args[0].value] = {}
         
-        #Code below handles creation of a mapping of user created objects and the AWS service they are connected to. 
-        response = getUserCreatedObj(node)
-
-        awsService = response[1]
-        userObj = response[0]
-
-        if awsService != None:
-            self.userObjDict[userObj] = awsService    
-
-            # return
-        if(node.value.func.value.id in self.userObjDict):
-            userObj = node.value.func.value.id
-            awsService = self.userObjDict[userObj]
-            
-
-
-            response = getCalledAWSMethod(node, awsService)
-        
-            calledMethod = response[0]
-            calledService = response[1]
-            methodARN = response[2]
-
-
-
-            if calledService not in self.calledMethodDict:
-                self.calledMethodDict[calledService] = {}
-
-                self.calledMethodDict[calledService][methodARN] = []
-
-                if calledMethod not in self.calledMethodDict[calledService][methodARN]:
-                    self.calledMethodDict[calledService][methodARN].append(calledMethod)
+                self.generic_visit(node)
 
             else:
-                if methodARN not in self.calledMethodDict[calledService]:
-                    self.calledMethodDict[calledService][methodARN] = []
+                callingUserObj = node.value.func.value.id
 
-                    # print(calledMethod)
-                    # if calledMethod not in self.calledMethodDict[calledService][methodARN]:
-                    #     self.calledMethodDict[calledService][methodARN].append(calledMethod)
+                if(callingUserObj in self.userObjDict):
 
-                self.calledMethodDict[calledService][methodARN].append(calledMethod)
+                    awsMethod = node.value.func.attr
+
+                    keywords = node.value.keywords
+
+                    if node.value.keywords == []:
+                        nameArg = "*"
+                    else:
+                        for keyword in node.value.keywords:
+
+                            '''
+                            FunctionName refers to name argument for lambda 
+                            equivalent s3 argument is sometimes 'Bucket'
+                            '''
+                            if keyword.arg == 'FunctionName':
+                                nameArg = keyword.value.value
+                                '''
+                                session -> config ->
+                                Figure out how to translate testFunction to arn:aws:lambda:us-east-1:221094580673:function:testFunction
+                                '''
+
+
+                    awsService = self.userObjDict[callingUserObj]   
+                    
+                    if nameArg not in self.extractDict[awsService]:
+                        self.extractDict[awsService][nameArg] = []
+
+                    if(awsMethod not in self.extractDict[awsService][nameArg]):
+                        self.extractDict[awsService][nameArg].append(awsMethod)          
+
+        except AttributeError:
+            self.generic_visit(node)
+
 
         self.generic_visit(node)
 
-
-
-
     def report(self):
         # pprint(self.stats)
-        # pprint(self.userObjDict)
-        # pprint(self.calledMethodDict)
-
-        return self.calledMethodDict
-
+        pprint(self.userObjDict)
+        pprint(self.extractDict)
+        print()
+        return self.extractDict
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
