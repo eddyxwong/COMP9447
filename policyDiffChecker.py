14552b61fe4d2125abbc570ae6c7d7480137f413
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-07-19 13:49:14 +1000

    Github actions test

--- policyDiffChecker.py
+++ policyDiffChecker.py
@@ -3,6 +3,28 @@ import parliament
 import os
 import subprocess
 import shlex
+import pkg_resources
+
+
+iam_definition_path = pkg_resources.resource_filename(__name__, "iam_definition.json")
+iam_definition = json.load(open(iam_definition_path, "r"))
+
+
+def get_privilege_info(service, action):
+    """
+    Given a service, like "s3"
+    and an action, like "ListBucket"
+    return the info from the docs about that action, along with some of the info from the docs
+    """
+    for service_info in iam_definition:
+        if service_info["prefix"] == service:
+            for privilege_info in service_info["privileges"]:
+                if privilege_info["privilege"] == action:
+                    privilege_info["service_resources"] = service_info["resources"]
+                    privilege_info["service_conditions"] = service_info["conditions"]
+                    return privilege_info
+
+
 
 json_files = []
 
@@ -39,7 +61,11 @@ for dict in response['Policies']:
     shellresponse = subprocess.getoutput('parliament --file {}'.format(shlex.quote(jsonfile_name)))
     policy_actions= policyobj.get_allowed_actions()
     for action in policy_actions:
-        dict['Allowed Actions'].append(action)
+        actiondict = parliament.expand_action(action)[0]
+        info = get_privilege_info(actiondict['service'], actiondict['action'])
+        dict['Allowed Actions'].append("Action: "+ action)
+        dict['Allowed Actions'].append("Service Info: "+ info['description'])
+    print(shellresponse)
     dict['Findings'] = list(shellresponse.split("\n"))
 
 print(json.dumps(response, sort_keys=False, indent=4))
