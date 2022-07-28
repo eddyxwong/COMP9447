e926879c01a004f09d7e9b238eea65990897cefc
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-07-28 14:43:02 +1000

    test

--- policyDiffChecker.py
+++ policyDiffChecker.py
@@ -2,33 +2,12 @@ import json
 import parliament
 import os
 import subprocess
-import pkg_resources
 import shlex
 
 # Grab a txt file of json files found
 # Take json files and run parliament in github actions
 # Take parliament output and put that inside the dictionary
-
-
-iam_definition_path = pkg_resources.resource_filename(__name__, "iam_definition.json")
-iam_definition = json.load(open(iam_definition_path, "r"))
-
-
-# Helper Function To Get Priviledge info
-def get_privilege_info(service, action):
-    """
-    Given a service, like "s3"
-    and an action, like "ListBucket"
-    return the info from the docs about that action, along with some of the info from the docs
-    """
-    for service_info in iam_definition:
-        if service_info["prefix"] == service:
-            for privilege_info in service_info["privileges"]:
-                if privilege_info["privilege"] == action:
-                    privilege_info["service_resources"] = service_info["resources"]
-                    privilege_info["service_conditions"] = service_info["conditions"]
-                    return privilege_info
-
+# Make the pyton program take in the directory and scan it 
 
 json_files = []
 
@@ -44,6 +23,7 @@ for r, d, f in os.walk('TestIAMpolicies'):
 #From the policies, map it to the functions and list out what they can do
 response = {'Policies': []}
 
+# include reasoning why this is here
 shellresponse = subprocess.getoutput('parliament --directory {}'.format(shlex.quote('TestIAMpolicies'))).split('\n')
 
 for jsonfile_name in json_files:
