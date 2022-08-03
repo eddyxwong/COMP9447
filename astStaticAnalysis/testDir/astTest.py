94510cb748e0e60ee18be258dd727c76dfb949fa
Frank Su <franksu@y7mail.com>
2022-08-03 21:13:30 +1000

    demo

--- astStaticAnalysis/testDir/astTest.py
+++ astStaticAnalysis/testDir/astTest.py
@@ -5,10 +5,6 @@ clientLambda = boto3.client('lambda')
 s3 = boto3.client('s3')
 
 
-def listBuckets():
-    response = s3.list_buckets()
-    return response
-
 response = s3.create_bucket(Bucket='zachbucket222')
 
 response = s3.delete_bucket(Bucket='zachbucket222')
