ccd3b4586aa3a64b70eb4173ea718ba487528d5a
Frank Su <franksu@y7mail.com>
2022-08-03 21:11:46 +1000

    demo

--- astStaticAnalysis/testDir/astTest.py
+++ astStaticAnalysis/testDir/astTest.py
@@ -4,12 +4,6 @@ import boto3
 clientLambda = boto3.client('lambda')
 s3 = boto3.client('s3')
 
-def doSomething():
-    response = clientLambda.get_function( 
-            FunctionName='arn:aws:lambda:us-east-1:221094580673:function:testFunction',
-    )
-
-    print(response)
 
 def listBuckets():
     response = s3.list_buckets()
