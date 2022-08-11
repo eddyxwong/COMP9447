31b71be63389a64f69a28de4a4a4b569c0dec4b7
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-08-11 18:12:14 +1000

    removed frontend from setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
    version='1.0',
    description='A set-up file for github actions to run for automated least priviledge enforcement',
    author='Zachary Ngooi',
-   packages=['astStaticAnalysis', 'AWSPlayground', 'tests', 'frontend'],  #Defines user modules to be installed
+   packages=['astStaticAnalysis', 'AWSPlayground', 'tests'],  #Defines user modules to be installed
 )
 
