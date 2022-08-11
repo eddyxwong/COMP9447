d52bb95a3aa0859ed506fbb34f8181b7198faa71
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-08-11 13:20:55 +1000

    Cleaned up more things, checking if anything breaks

--- astStaticAnalysis/policyDiffChecker.py
+++ astStaticAnalysis/policyDiffChecker.py
@@ -12,7 +12,7 @@ Args:
         directory (str): a string of directory name
 
     Returns:
-        dictionary(differences): Dictionary of findings and differences in content for policys
+        response (dict): Dictionary of findings and differences in content for policys
 
 '''
 def main(argv):
@@ -33,16 +33,15 @@ def main(argv):
 
 
 
-
 """Given a list of commandline arguments, checks for any input error and informs the user if any are found
 
     Args:
-        argument(list): a list of command line arguments
+        argument (list): a list of command line arguments
 
     Returns:
-        (str): string of error message
+        NA
 """
-def inputCheck(argument: list) -> str:
+def inputCheck(argument: list):
     if len(argument) > 2 or len(argument) < 2:
         print("Incorrect input, too many/too little arguments provided.\nPlease follow the format:\n'python policyDiffchecker.py directory_name'")
         exit()
@@ -51,10 +50,10 @@ def inputCheck(argument: list) -> str:
 """Given a directory, traverses it and finds the relative path to all .json files before appending to a list
 
     Args:
-        directory(str): a string of directory name
+        directory (str): a string of directory name
 
     Returns:
-        json_files(list): list of .json files inside said directory
+        json_files (list): list of .json files inside said directory
 """
 def folderWalker(directory: str) -> list:
     json_files = []
@@ -74,7 +73,7 @@ def folderWalker(directory: str) -> list:
         json_files (list): List of json files found inside the directory
 
     Returns:
-        dictionary(dict): list of .json files inside said directory
+        dictionary (dict): list of .json files inside said directory
 """
 def directInit(dictionary: dict, json_files: list) -> dict:
     for jsonfile_name in json_files:
@@ -87,20 +86,18 @@ def directInit(dictionary: dict, json_files: list) -> dict:
     return dictionary
 
 
-"""
-    Given a previously created base template for displaying IAM policies, add the corresponding findings to
-    the template.
+"""Given a previously created base template for displaying IAM policies, add the corresponding findings to
+    a base template to display differences found.
 
     Args:
-        shellresponse (list): Findings for all json files within the directory specified by the user based on 
-        the tool, parliament.
-        template (dict): Base template to display all findings and differences.
+        shellresponse (list): Findings for all json files within the directory specified by the user based on the tool, Parliament.
         json_files (list): List of json files found inside the directory specified by the user.
 
     Returns:
         template(dict): Template that has been filled with findings and differences between policies
 """
 def diffCheck(shellresponse: list, json_files: list) -> dict:
+
     template = {'Policies': []}
 
     for jsonfile_name in json_files:
@@ -110,20 +107,28 @@ def diffCheck(shellresponse: list, json_files: list) -> dict:
             'Differences': {}
         })
 
+    for jsonDict in template['Policies']:
 
+        jsonfile_name = jsonDict['Policy Name']
 
-    for dict in template['Policies']:
+        jsonDict = appendFindings(shellresponse, jsonDict, jsonfile_name)
 
-        jsonfile_name = dict['Policy Name']
-
-        appendFindings(shellresponse, dict, jsonfile_name)
-
-        appendDifferences(dict, json_files, jsonfile_name)
+        jsonDict = appendDifferences(json_files, jsonDict, jsonfile_name)
        
     return template
 
 
-def appendFindings(shellresponse: list, dictionary: dict, jsonfile_name: str):
+"""Appends findings by Parliment to the correct position in the dictionary given.
+
+    Args:
+        shellresponse (list): Findings for all json files within the directory specified by the user based on the tool, Parliament.
+        dictionary (dict): Dictionary for each json file within the template.
+        jsonfile_name (str): a string of a json file name.
+
+    Returns:
+        dictionary (dict): Filled dictionary of findings found for the particular json file.
+"""
+def appendFindings(shellresponse: list, dictionary: dict, jsonfile_name: str) -> dict:
     for pathing in shellresponse:
         reformed_findings = str(pathing.replace(r'\\', '/'))
         if jsonfile_name in reformed_findings:
@@ -132,24 +137,45 @@ def appendFindings(shellresponse: list, dictionary: dict, jsonfile_name: str):
     if len(dictionary['Findings']) == 0:
             dictionary['Findings'].append('No faults found within policy')
 
-def appendDifferences(dictionary: dict, json_files: list, jsonfile_name: str):
-    if len(json_files) > 1:
-        for jsonfile_name2 in json_files:
-            filename1 = jsonfile_name
-            filename2 = jsonfile_name2
-            if filename1 == filename2:
-                continue
-            file1contents = set(open(filename1).readlines())
-            file2contents = set(open(filename2).readlines())
-            if file1contents == file2contents:
-                print("Yup they're the same!")
-            else:
-                dictionary['Differences'].update({"Contents found in "+filename1+" but not in "+filename2: []})
-                for diffLine in file1contents - file2contents:
-                    diffLine = (diffLine).replace(" ", "").replace('\n','').replace(',','').replace('}', '').replace('"', '')
-                    if diffLine != '':
-                        dictionary['Differences']["Contents found in "+filename1+" but not in "+filename2].append(str(diffLine))
+    return dictionary
 
+
+"""Appends differences in contents of IAM policy json files to the correct position in the dictionary given.
+
+    Args:
+        json_files (list): List of all json files previously found in the directory.
+        dictionary (dict): Dictionary for each json file within the template.
+        jsonfile_name (str): a string of a json file name.
+
+    Returns:
+        dictionary (dict): Filled dictionary of differences found for the json files in the directory.
+"""
+def appendDifferences(json_files: list, dictionary: dict, jsonfile_name: str) -> dict:
+    if len(json_files) <= 1:
+        return dictionary
+
+    # Run through list to compare while ignoring the json_filename
+    for jsonNameCrawl in json_files:
+        if jsonfile_name == jsonNameCrawl:
+            continue
+
+        filename1 = jsonfile_name
+        filename2 = jsonNameCrawl
+
+        # Create sets from both file contents
+        file1contents = set(open(filename1).readlines())
+        file2contents = set(open(filename2).readlines())
+
+        # Check for differences
+        if file1contents == file2contents:
+            print("Yup they're the same!")
+        else:
+            dictionary['Differences'].update({"Contents found in "+filename1+" but not in "+filename2: []})
+            for diffLine in file1contents - file2contents:
+                diffLine = (diffLine).replace(" ", "").replace('\n','').replace(',','').replace('}', '').replace('"', '')
+                if diffLine != '':
+                    dictionary['Differences']["Contents found in "+filename1+" but not in "+filename2].append(str(diffLine))
+    return dictionary
 
 if __name__ == "__main__":
     main(sys.argv)
