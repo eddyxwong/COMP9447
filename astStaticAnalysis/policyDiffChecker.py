50fa122c6a7a9d30ffe63038095a7d062d9b84eb
Zachary Ngooi <zacharyngooi@hotmail.com>
2022-08-11 11:43:45 +1000

    Cleaned up more things, checking if anything breaks

--- astStaticAnalysis/policyDiffChecker.py
+++ astStaticAnalysis/policyDiffChecker.py
@@ -3,45 +3,66 @@ import os
 import sys
 import subprocess
 import shlex
-import parliament
 
-# Grab a txt file of json files found
-# Take json files and run parliament in github actions
-# Take parliament output and put that inside the dictionary
-# Make the pyton program take in the directory and scan it 
+'''
+IAM Policy Comparison Program, find all json files in a given directory and compare them based on findings 
+from a third-party tool (parliament) and differences in their contents
+
+Args:
+        directory (str): a string of directory name
+
+    Returns:
+        dictionary(differences): Dictionary of findings and differences in content for policys
+
+'''
 
 
 def main(argv):
+    if len(argv) > 2:
+        print("Too many arguments, please input only one directory name")
+        exit()  
     directory = argv[1].strip(".\\")
 
-    json_files = []
-
-    json_files = folderWalker(directory, json_files)
-
-    #From the policies, map it to the functions and list out what they can do
-    response = {'Policies': []}
+    json_files = folderWalker(directory)
 
     # include reasoning why this is here
 
     shellresponse = subprocess.getoutput('parliament --directory {}'.format(shlex.quote(directory))).split('\n')
     
+    response = {'Policies': []}
     response = directInit(response, json_files)
 
     response = diffcheck(shellresponse, response, json_files)
 
     print(json.dumps(response, sort_keys=False, indent=4))
 
-def folderWalker(directory: str, jsonList: list) -> list:
+def folderWalker(directory: str) -> list:
+    """Given a directory, traverses it and finds the relative path to all .json files before appending to a list
 
+    Args:
+        directory (str): a string of directory name
+
+    Returns:
+        List(json_files): list of .json files inside said directory
+    """
+    json_files = []
     # r=root, d=directories, f = files
     for r, d, f in os.walk(directory):
         for file in f:
                 if file.endswith(".json"):
                     abspath = os.path.join(r, file)
-                    jsonList.append(str(os.path.relpath(abspath).replace(os.path.sep, '/')))
-    return jsonList
+                    json_files.append(str(os.path.relpath(abspath).replace(os.path.sep, '/')))
+    return json_files
     
 def directInit(response: dict, json_files: list) -> dict:
+    """Given a dictionary, traverses it and finds the relative path to all .json files before appending to a list
+
+    Args:
+        directory (str): a string of directory name
+
+    Returns:
+        List(ast): list of .json files inside said directory
+    """
     for jsonfile_name in json_files:
         response['Policies'].append({
             'Policy Name': jsonfile_name,
